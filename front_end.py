dbec37080d0761939409d382182446b2c6afa989
Alexander Wong <alexander.wong@oliverwyman.com>
2021-04-12 14:12:13 +0800

    running version of streamlit

--- front_end.py
+++ front_end.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import shap
 import streamlit as st
+import streamlit.components.v1 as components
 
 from src.data_transformations import classify_success
+from src.model import PlaylistSuccessPredictor, ShapObject
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 @st.cache
@@ -18,6 +23,17 @@ def load_success_data(base_frame, users_threshold, success_threshold):
     )
 
 
+@st.cache
+def create_shap_object(base_values, data, values, feature_names):
+    ShapObject(base_values, values, feature_names, data)
+    return ShapObject(base_values, values, feature_names, data)
+
+
+def st_shap(plot, height=None):
+    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+    components.html(shap_html, height=height)
+
+
 def main():
     st.title("What Makes a Playlist Successful?")
     st.write("Creator: Alexander Wong")
@@ -26,6 +42,9 @@ def main():
     base_frame = load_base_data()
 
     # Sidebar Inputs -------------------------------------------------------------------------------------------------
+    genre_options = sorted(base_frame["genre_1"].unique())
+    genre = st.sidebar.selectbox("Select genre:", options=genre_options)
+
     users_threshold = st.sidebar.number_input(
         "Minimum monthly number of Users:",
         min_value=10,
@@ -33,15 +52,50 @@ def main():
     success_threshold = st.sidebar.slider(
         "Streaming-ratio success threshold (%):", min_value=1, max_value=99, value=75
     )
-    model_test_size = st.sidebar.slider(
-        "Model train-test split (%):", min_value=1, max_value=99, value=80
-    )
+    # model_train_size = (
+    #         st.sidebar.slider(
+    #             "Model train-test split (%):", min_value=1, max_value=99, value=80
+    #         )
+    #         / 100
+    # )
 
     # ----------------------------------------------------------------------------------------------------------------
-    success_frame = load_success_data(
-        base_frame, users_threshold, success_threshold
-    )
-    st.write("Data has been loaded")
+    if st.button("Click to train model"):
+        labelled_frame = load_success_data(
+            base_frame, users_threshold, success_threshold
+        )
+
+        PS = PlaylistSuccessPredictor(labelled_frame, genre)
+        PS.train_model(0.80, n_estimators=1000)
+        test_accuracy, baseline_accuracy = PS.compute_accuracy()
+
+        st.write(f"Model test accuracy: {test_accuracy}%")
+        st.write(f"Baseline accuracy: {baseline_accuracy}%")
+
+        # SHAP feature importance
+        explainer = shap.TreeExplainer(PS.model)
+        shap_values = explainer.shap_values(PS.X_train.to_numpy())
+
+        st.header("SHAP Feature Importance")
+        st.pyplot(shap.summary_plot(shap_values, PS.X_test, plot_type='bar'))
+
+        st.pyplot(shap.summary_plot(shap_values, PS.X_train, show=True))
+
+        st.write("DONE!")
+
+    # Individual observation
+    # row = int(st.number_input(
+    #     "Observation number to inspect",
+    # ))
+    #
+    # shap_object = ShapObject(
+    #     base_values=explainer.expected_value,
+    #     values=explainer.shap_values(PS.X_train)[row, :],
+    #     feature_names=PS.X_train.columns,
+    #     data=PS.X_train.iloc[row, :],
+    # )
+    #
+    # st_shap(shap.waterfall_plot(shap_object))
 
 
 if __name__ == "__main__":
---
+++ src/model.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import xgboost
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import accuracy_score
+
+from src.constants import MODEL_NUMERICAL_FEATURES, MODEL_CATEGORICAL_FEATURES
+
+
+class PlaylistSuccessPredictor:
+    def __init__(self, frame, genre):
+        self.genre = genre
+        self.genre_frame = frame.loc[lambda f: f["genre_1"] == genre].dropna()
+        self.dummies_frame = pd.get_dummies(
+            self.genre_frame[
+                ["success_streaming_ratio_users", "playlist_uri"]
+                + MODEL_NUMERICAL_FEATURES
+                + MODEL_CATEGORICAL_FEATURES
+            ],
+            columns=MODEL_CATEGORICAL_FEATURES,
+        )
+        self.success_frame = self.dummies_frame.loc[
+            lambda f: f["success_streaming_ratio_users"] == 1
+        ]
+        self.non_success_frame = self.dummies_frame.loc[
+            lambda f: f["success_streaming_ratio_users"] == 0
+        ]
+        self.test_frame = None
+        self.train_frame = None
+        self.X_train = None
+        self.y_train = None
+        self.shap_frame = None
+        self.X_test = None
+        self.y_test = None
+        self.model = None
+        self.y_pred = None
+
+    def train_model(self, train_size, n_estimators):
+        test_size = int((1 - train_size) * len(self.success_frame))
+
+        test_frame_success = self.success_frame.sample(
+            n=test_size, replace=False, random_state=69
+        )
+        test_frame_non_success = self.non_success_frame.sample(
+            n=test_size, replace=False, random_state=69
+        )
+
+        train_frame_success = self.success_frame.loc[
+            lambda f: ~f["playlist_uri"].isin(test_frame_success["playlist_uri"])
+        ]
+        train_frame_non_success = self.non_success_frame.loc[
+            lambda f: ~f["playlist_uri"].isin(test_frame_non_success["playlist_uri"])
+        ]
+
+        self.test_frame = pd.concat(
+            [test_frame_success, test_frame_non_success]
+        ).reset_index(drop=True)
+        self.train_frame = pd.concat(
+            [train_frame_success, train_frame_non_success]
+        ).reset_index(drop=True)
+
+        # Upsample the imbalanced success class until equal number of training observations
+        sm = SMOTE(random_state=69)
+        x_drop_cols = ["success_streaming_ratio_users", "playlist_uri"]
+        self.X_train, self.y_train = sm.fit_resample(
+            self.train_frame.drop(columns=x_drop_cols),
+            self.train_frame["success_streaming_ratio_users"],
+        )
+        self.shap_frame = pd.merge(
+            self.X_train, self.y_train, left_index=True, right_index=True
+        )
+
+        self.X_test = self.test_frame.drop(columns=x_drop_cols)
+        self.y_test = self.test_frame["success_streaming_ratio_users"]
+
+        self.model = xgboost.XGBClassifier(
+            n_estimators=n_estimators, verbosity=0, n_jobs=-1
+        ).fit(self.X_train, self.y_train)
+
+    def compute_accuracy(self):
+        self.y_pred = self.model.predict(self.X_test)
+        test_accuracy = round(accuracy_score(self.y_test, self.y_pred) * 100, 1)
+
+        y_baseline_preds = np.zeros(len(self.y_test))
+        baseline_accuracy = round(
+            accuracy_score(self.y_test, y_baseline_preds) * 100, 1
+        )
+        return test_accuracy, baseline_accuracy
+
+
+class ShapObject:
+    def __init__(self, base_values, data, values, feature_names):
+        self.base_values = base_values  # Single value
+        self.data = data  # Raw feature values for 1 row of data
+        self.values = values  # SHAP values for the same row of data
+        self.feature_names = feature_names  # Column names
